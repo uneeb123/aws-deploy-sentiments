4331fab89f446a17ed9d7a8bfc23503443617c16
uneeb123 <uneeb.agha@gmail.com>
2017-12-04 14:38:10 -0800

    cf template to spin up ec2 instance

--- deploy.rb
+++ deploy.rb
@@ -2,9 +2,14 @@ require 'aws-sdk'
 require 'json'
 
 CREDENTIALS_FILE = 'credentials.json'.freeze
+CLOUDFORMATION_TEMPLATE_FILE = 'stack.template'.freeze
 
 def get_credentials_from_file
   JSON.parse(File.read(CREDENTIALS_FILE))
+end
+
+def get_template_from_file
+  File.read(CLOUDFORMATION_TEMPLATE_FILE)
 end
 
 @credentials = get_credentials_from_file
@@ -13,3 +18,12 @@ Aws.config.update({
   region: 'us-west-2',
   credentials: Aws::Credentials.new(@credentials['akid'], @credentials['secret'])
 })
+
+client = Aws::CloudFormation::Client.new
+
+resp = client.create_stack({
+  stack_name: 'TestStack',
+  template_body: get_template_from_file,
+})
+
+puts resp.stack_id
